8f97d3fcc15c9fd67bd6ef427c1465acb52a9436
YoGhurt <623770278@qq.com>
2016-12-16 10:41:23 +0800

    some example for studying Android

---
+++ ActivityTest/app/src/main/java/yoghurt/activitytest/FirstActivity.java
@@ -0,0 +1,67 @@
+package yoghurt.activitytest;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static yoghurt.activitytest.R.layout.first_layout;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                /*显式Intent调用SecondActivity
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);*/
+                /*隐式Intent调用SecondActivity
+                Intent intent = new Intent("yoghurt.activitytest.ACTION_START");
+                intent.addCategory("yoghurt.activitytest.MY_CATEGORY");*/
+                /*打开网页或调用ThrdActivity
+                Intent intent = new Intent((Intent.ACTION_VIEW));
+                intent.setData(Uri.parse("http://www.baidu.com"));*/
+                /*拨打电话
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));*/
+                /*向SecondActivity传递数据
+                String data = "Hello SecondActivity";
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                intent.putExtra("extra_data",data);*/
+                //startActivity(intent);
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+    }
+}
---
+++ ActivityTest/app/src/main/java/yoghurt/activitytest/SecondActivity.java
@@ -0,0 +1,19 @@
+package yoghurt.activitytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        /*由FirstActivity传来数据
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("extra_data");
+        Log.d("SecondActivity",data);*/
+    }
+}
