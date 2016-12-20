bfbf9c7b26b41224a7a27a81943f628cf328187f
YoGhurt <623770278@qq.com>
2016-12-20 15:23:16 +0800

    Android UI布局的学习

--- UIWidgetTest/app/src/main/java/yoghurt/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/yoghurt/uiwidgettest/MainActivity.java
@@ -1,22 +1,63 @@
 package yoghurt.uiwidgettest;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Process;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private EditText editText;
+    private ImageView imageView;
+    private ProgressBar progressBar;
+
     //使用接口的方式来注册监听器
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.button:
-                String inputText = editText.getText().toString();
-                Toast.makeText(MainActivity.this,inputText,Toast.LENGTH_SHORT).show();
+                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setTitle("This is ProgressDialog");
+                progressDialog.setMessage("Loading...");
+                progressDialog.setCancelable(true);
+                progressDialog.show();
+                /*AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("This is dialog");
+                dialog.setCancelable(false);
+                dialog.setMessage("Something Important.");
+                dialog.setPositiveButton("ok",new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog,int which){
+                    }
+                });
+                dialog.setNegativeButton("Cancel",new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog,int which){
+
+                    }
+                });
+                dialog.show();*/
+                /*int progress = progressBar.getProgress();
+                progress = progress + 10;
+                progressBar.setProgress(progress);*/
+                /*if(progressBar.getVisibility() == View.GONE) {
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+                else {
+                    progressBar.setVisibility(View.GONE);
+                }*/
+                /*String inputText = editText.getText().toString();
+                Toast.makeText(MainActivity.this,inputText,Toast.LENGTH_SHORT).show();*/
+               //imageView.setImageResource(R.drawable.jpg1);
+
                 break;
             default:
                 break;
@@ -29,6 +70,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button button = (Button)findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.image_view);
+        progressBar = (ProgressBar)findViewById(R.id.process_bar);
         button.setOnClickListener(this);
         /*使用匿名类的方法来注册Button Listener
         button.setOnClickListener(new View.OnClickListener() {
