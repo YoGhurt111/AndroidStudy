4d91547a2270bf05b9a6abf032ce0764eda2695d
YoGhurt <623770278@qq.com>
2017-01-02 20:26:09 +0800

    Android之Fragment的学习

---
+++ FragmentTest/app/src/main/java/yoghurt/fragmenttest/LeftFragment.java
@@ -0,0 +1,18 @@
+package yoghurt.fragmenttest;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LeftFragment extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.left_fragment,container,false);
+        return view;
+    }
+}
---
+++ FragmentTest/app/src/main/java/yoghurt/fragmenttest/RightFragment.java
@@ -0,0 +1,22 @@
+package yoghurt.fragmenttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Dell on 2017/1/2.
+ */
+
+public class RightFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.right_fragment,container,false);
+        return view;
+    }
+}
